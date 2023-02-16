9856e99043bfa67a0af57f6b220e387acd8800a1
Belochkln <120090699+Belochkln@users.noreply.github.com>
2023-02-16 19:47:39 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символам
+string[] array = { "=)", "Geek", "123", "two", "+-*/=" };
+string word;
+
+for (int i = 0; i < array.Length; i++)
+{
+    word = array[i];
+    if (word.Length <= 3)
+    {
+        Console.Write(word + " ");
+    }
+}
